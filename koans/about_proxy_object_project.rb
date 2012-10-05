8db3584877008a04995c381f48f56c89a0ca17c9
Charles Munger <charleslmunger@gmail.com>
2012-10-05 02:26:28 -0700

    Reached enlightenment.

--- koans/about_proxy_object_project.rb
+++ koans/about_proxy_object_project.rb
@@ -13,12 +13,29 @@ require File.expand_path(File.dirname(__FILE__) + '/edgecase')
 # of the Proxy class is given in the AboutProxyObjectProject koan.
 
 class Proxy
+  attr_reader :messages
   def initialize(target_object)
     @object = target_object
+    @messages = []
     # ADD MORE CODE HERE
   end
 
-  # WRITE CODE HERE
+  def respond_to?(symbol)
+    super(symbol)
+  end
+  
+  def method_missing(symbol, *args)
+    @messages << symbol
+    @object.__send__(symbol,*args)
+  end
+  
+  def called?(symbol)
+    @messages.include?(symbol)
+  end
+  
+  def number_of_times_called(symbol)
+    @messages.count(symbol)
+  end
 end
 
 # The proxy object should pass the following Koan:
